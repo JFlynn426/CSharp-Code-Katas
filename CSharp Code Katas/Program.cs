9ea82a9bfeb4caef60dd6693771064d47aaac73c
Jflynn426 <jflynnpics@gmail.com>
2019-05-15 04:46:48 -0400

    divisible

--- CSharp Code Katas/Program.cs
+++ CSharp Code Katas/Program.cs
@@ -103,6 +103,16 @@ namespace CSharp_Code_Katas
                 return s;
             }
         }
-    }
+        public class DivisibleNb
+        {
+            public static bool isDivisible(long n, long x, long y)
+            {
 
+                if (n % x == 0 && n % y == 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
     }
+}
