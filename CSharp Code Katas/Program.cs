4a0e32f3defaa44bea78007c630426912faac687
Jflynn426 <jflynnpics@gmail.com>
2019-10-08 21:45:32 -0400

    are they the same

--- CSharp Code Katas/Program.cs
+++ CSharp Code Katas/Program.cs
@@ -161,5 +161,21 @@ namespace CSharp_Code_Katas
 
             }
         }
+        class AreTheySame
+        {
+            public static bool comp(int[] a, int[] b)
+            {
+                if ((a == null) || (b == null))
+                {
+                    return false;
+                }
+
+                int[] copy = a.Select(x => x * x).ToArray();
+                Array.Sort(copy);
+                Array.Sort(b);
+
+                return copy.SequenceEqual(b);
+            }
+        }
     }
 }
