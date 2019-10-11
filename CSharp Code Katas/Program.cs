81bac61653ffad0fbea310df34e9723bf8aab364
Jflynn426 <jflynnpics@gmail.com>
2019-10-10 21:20:32 -0400

    two sum

--- CSharp Code Katas/Program.cs
+++ CSharp Code Katas/Program.cs
@@ -197,5 +197,17 @@ namespace CSharp_Code_Katas
             }
 
         }
+        public class Kata0
+        {
+            public static int[] TwoSum(int[] numbers, int target)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                    for (int j = i + 1; j < numbers.Length; j++)
+                        if (numbers[i] + numbers[j] == target)
+                            return new int[] { i, j };
+
+                return new int[0];
+            }
+        }
     }
 }
